e29af3a800afda1ae8e85b61bfab8b450de2f53e
CDouglas1029 <cdouglaslim@gmail.com>
2024-12-15 11:07:08 -0500

    Add handler for processing receipts

---
+++ handlers/process.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"receipt-processor-challenge/models"
+	"receipt-processor-challenge/storage"
+	"receipt-processor-challenge/utils"
+)
+
+func ProcessReceipt(w http.ResponseWriter, r *http.Request) {
+	var receipt models.Receipt
+	err := json.NewDecoder(r.Body).Decode(&receipt)
+	if err != nil {
+		http.Error(w, "Invalid receipt format. Please verify input", http.StatusBadRequest)
+		return
+	}
+
+	id := utils.GenerateID()
+	points := models.CalculatePoints(receipt)
+
+	storage.StoreReceipt(id, points)
+
+	response := map[string]string{"id": id}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
