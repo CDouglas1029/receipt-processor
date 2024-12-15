5af9e57c7a8c24781a621bf5fcdd6ea5ba2a5a86
CDouglas1029 <cdouglaslim@gmail.com>
2024-12-15 11:20:55 -0500

    Complete main funcionality

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+	"receipt-processor-challenge/handlers"
 
-	"github.com/gorilla/mux" // Correct import path
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
+	router.HandleFunc("/receipts/process", handlers.ProcessReceipt).Methods("POST")
+	router.HandleFunc("/receipts/{id}/points", handlers.GetPoints).Methods("GET")
 
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
